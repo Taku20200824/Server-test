5506c9b6b85e7ea588e0b1dbfd3cc43c78ec3f2f
Taku1220 <152232473+Taku20200824@users.noreply.github.com>
2026-08-20 14:58:04 +0900

    Remove old Isle app routes

--- src/app/discord/page.tsx
+++
@@ -1,8 +0,0 @@
-import { DiscordPageContent } from "@/components/discord-page-content";
-import { getDiscordCommunity } from "@/lib/integrations/discord";
-
-export default async function DiscordPage() {
-  const discord = await getDiscordCommunity();
-
-  return <DiscordPageContent discord={discord} />;
-}
