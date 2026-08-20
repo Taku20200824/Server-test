8d29ca7d425eeddf5d18ece56d49fab2a95f515c
Taku1220 <152232473+Taku20200824@users.noreply.github.com>
2026-08-20 14:58:25 +0900

    Remove old Isle app routes

--- src/app/map/page.tsx
+++
@@ -1,8 +0,0 @@
-import { MapPageContent } from "@/components/map-page-content";
-
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
-
-export default async function MapPage() {
-  return <MapPageContent />;
-}
